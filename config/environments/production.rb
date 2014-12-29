1716b484c7fade6d4ddb6626d935e32b3e4d5940
Lainie Rose Wright <wright229@purdue.edu>
2014-12-29 00:38:17 -0600

    Modify Gemfile.lock

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,13 @@ Pinteresting::Application.configure do
   # Required for Heroku
   # Note to set this to actual host
   config.action_mailer.default_url_options = { :host => 'https://glacial-plains-4091.herokuapp.com/'}
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
